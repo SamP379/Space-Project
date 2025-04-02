61bc87a85dd7fc43562ca68063e7295e32b327d8
Sam Patel <sampatel379@gmail.com>
2025-04-02 19:50:41 +0100

    Added code to find a user and the iss

--- iss.py
+++ iss.py
@@ -1,14 +1,35 @@
+
+
 import requests
 from geopy.geocoders import Nominatim
 
-
 ISS_ENDPOINT = "http://api.open-notify.org/iss-now.json"
 
+def get_address_position(address : str):
+    geolocator = Nominatim(user_agent = "locator")
+    location = geolocator.geocode(address)
+    if location is None:
+        return None
+    position = location[-1]
+    return position
 
-def get_iss_position(api_endpoint: str) -> tuple:
-    """Returns a tuple which holds the latitude and longitude of the International Space Station"""
+
+def locate_user():
+    print("") # TODO Fix this bug
+    user_address = input("Enter your address: ")
+    print("Locating your position...")
+    user_position = get_address_position(user_address)
+    if user_position is None:
+        print("Unable to find your position.")
+        return None
+    else:
+        print(f"Your position is: {user_position}")
+        return user_position
+    
+
+def get_iss_position(endpoint: str) -> tuple:
     try:
-        response = requests.get(url = api_endpoint)
+        response = requests.get(url = endpoint)
         content = response.json()
         iss_position = (float(content["iss_position"]["latitude"]), float(content["iss_position"]["longitude"]))
         return iss_position
@@ -17,36 +38,66 @@ def get_iss_position(api_endpoint: str) -> tuple:
         return None
 
 
-def get_position(address : str) -> tuple:
-    """Returns a tuple which holds the latitude and longitude of an address given as a string"""
-    geolocator = Nominatim(user_agent = "locator")
-    location = geolocator.geocode(address)
-    position = location[-1]
-    return position
+def locate_iss():
+    print("\nLocating the position of the International Space Station...")
+    iss_position = get_iss_position(ISS_ENDPOINT)
+    if iss_position is None:
+        return None
+    else:
+        print(f"The position of the International Space Station is: {iss_position}\n")
+        # TODO "The International Space Station is somewhere over Ohio, USA"
+        return iss_position
 
 
-# Get the address of a given set of coordinates
-def get_address(position : str):
-    geolocator = Nominatim(user_agent = "locator")
-    location = geolocator.reverse(position)
-    address = location.address
-    return address
+def find_distance():
+
+    user_position = locate_user()
+    if user_position is None:
+        return
+    
+    iss_position = locate_iss()
+    if iss_position is None:
+        return
+
+    else:
+        print("Calculating distance...")
+        #distance = haversine_distance(user_position, iss_position)
+        #print(f"The International Space Station is about {distance} KM from you")
 
 
-from math import cos, asin, sqrt, pi
 
-def haversine_distance(coordinates1 : tuple, coordinates2 : tuple):
-    lat1 = coordinates1[0]
-    lon1 = coordinates1[1]
-    lat2 = coordinates2[0]
-    lon2 = coordinates2[1]
 
-    r = 6371 # km
-    p = pi / 180
 
-    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
-    return 2 * r * asin(sqrt(a))
-# https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+
+
+
+
+
+
+
+
+# # Get the address of a given set of coordinates
+# def get_address(position : str):
+#     geolocator = Nominatim(user_agent = "locator")
+#     location = geolocator.reverse(position)
+#     address = location.address
+#     return address
+
+
+# from math import cos, asin, sqrt, pi
+
+# def haversine_distance(coordinates1 : tuple, coordinates2 : tuple):
+#     lat1 = coordinates1[0]
+#     lon1 = coordinates1[1]
+#     lat2 = coordinates2[0]
+#     lon2 = coordinates2[1]
+
+#     r = 6371 # km
+#     p = pi / 180
+
+#     a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
+#     return 2 * r * asin(sqrt(a))
+# # https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
 
 
 
@@ -65,39 +116,5 @@ Sample output:
     
     Calculating distance...
     The International Space Station is about 4320 KM from you
-
-
-    
-
-ISS MODULE
-
-
-
-
-def locate_user():
-    user_address = input("Enter your address: ")
-    print("Locating your position...")
-    user_position = get_address_position(user_address)
-    if user_position is None:
-        return None
-    else:
-        print("Your position is: {user_position})
-        return user_position
-
-
-find_distance():
-
-    user_position = locate_user()
-    iss_position = locate_iss()
-
-    if user_position is None or iss_position is None:
-        print("An error occured. Please try again later)
-    else:
-        print("Calculating distance...")
-        distance = haversine_distance(user_position, iss_position)
-        print(f"The International Space Station is about {distance} KM from you)
-
-        
-
 
 """
