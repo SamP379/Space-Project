e0f34870608c8c17725befdbf03bab3883c4671b
Sam Patel <sampatel379@gmail.com>
2025-04-03 19:33:19 +0100

    Update

--- main.py
+++ main.py
@@ -1,86 +1,42 @@
+
 """
 
+
+
 import time
-import requests
 import distance
-from math import cos, asin, sqrt, pi
-from geopy.geocoders import Nominatim
 
 
-ISS_ENDPOINT = "http://api.open-notify.org/iss-now.json"
 
 
-def get_address_position(address : str):
-    geolocator = Nominatim(user_agent = "locator")
-    location = geolocator.geocode(address)
-    if location is None:
-        return None
-    position = location[-1]
-    return position
+def main():
 
+    user_position = (52.003854, -0.776971)
 
-def locate_user():
-    print("") # TODO Fix this bug
-    user_address = input("Enter your address: ")
-    print("Locating your position...")
-    time.sleep(4)
-    user_position = get_address_position(user_address)
-    if user_position is None:
-        print("Unable to find your position.")
-        return None
+    iss = ISS()
+
+    if iss.was_found():
+        iss.display_status()
+        distance = self.calculate_distance()
+        iss.distance_between()
     else:
-        print(f"Your position is: {user_position}")
-        return user_position
-    
+        print("Unable to locate the ISS")
+            
+
+         
+"""
 
 
 
 
-def locate_iss():
-    print("\nLocating the position of the International Space Station...")
-    time.sleep(4)
-    iss_position = get_iss_position(ISS_ENDPOINT)
-    if iss_position is None:
-        return None
-    else:
-        print(f"The position of the International Space Station is: {iss_position}")
-        # TODO "The International Space Station is somewhere over Ohio, USA"
-        return iss_position
-
-
-def get_iss_speed():
-    iss_position1 = get_iss_position(ISS_ENDPOINT)
-    timestamp1 = time.time()
-    time.sleep(5)
-    iss_position2 = get_iss_position(ISS_ENDPOINT)
-    timestamp2 = time.time()
-    if iss_position1 is None or iss_position2 is None:
-        return None
-    else:
-        distancekm = distance.haversine(iss_position1, iss_position2)
-        time_secs = timestamp2 - timestamp1
-        speed = distancekm / time_secs
-        return round(speed)
-
-
-
-
-
-def find_distance():
-
-    user_position = locate_user()
-    if user_position is None:
-        return
-
-    iss_position = locate_iss()
-    if iss_position is None:
-        return
-
-    else:
-        print("\nCalculating distance...")
-        time.sleep(4)
-        distance = distance.haversine(user_position, iss_position)
-        speed = get_iss_speed()
-        print(f"The International Space Station is about {distance} km from you")
-        if speed is not None:
-            print(f"It is travelling at a speed of about {speed} km per second\n")"""
+# Enter your address: 47 Chipping Vale, Emerson Valley, Milton Keynes
+# Locating your position...
+# Your position is: (53.234, -723434)
+     
+# Locating the position of the International Space Station...
+# The position of the International Space Station is: (3.1224, -1.1324)
+# The International Space Station is somewhere over Ohio, USA
+# It is moving at a speed of about 7 km per second
+     
+# Calculating distance...
+# The International Space Station is about 4320 KM from you
