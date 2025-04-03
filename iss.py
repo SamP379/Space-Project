6ce75f10b36341e1de83028392439b8a000075e7
Sam Patel <sampatel379@gmail.com>
2025-04-03 19:34:23 +0100

    Update

--- iss.py
+++ iss.py
@@ -1,4 +1,6 @@
-"""import requests
+"""
+import time
+import requests
 
 
 class ISS:
@@ -8,6 +10,7 @@ class ISS:
     
     def __init__(self):
         self._position = self.get_latest_position()
+        self._speed = self.get_latest_speed()
     
         
     def get_latest_position(self):
@@ -27,5 +30,63 @@ class ISS:
         
     def get_position(self):
         return self._position
+    
+        
+    def get_latest_speed(self):
+        position1 = self.get_latest_position()
+        timestamp1 = time.time()
+        time.sleep(5)
+        position2 = self.get_latest_position()
+        timestamp2 = time.time()
+        if position1 or position2 is None:
+            return None
+        else:
+            distance_km = distance.haversine(iss_position1, iss_position2)
+            time_secs = timestamp2 - timestamp1
+            speed = distance_km / time_secs
+            return round(speed)
 
+
+
+
+
+
+import time
+import distance
+
+
+class ISS:
+
+    API_ENDPOINT = "http://api.open-notify.org/iss-now.json"
+
+    def __init__(self):
+        self.position
+        self.speed
+    
+        
+    def was_found(self):
+        return self.position is not None and self.speed is not None
+    
+        
+    def display_status(self):
+        if self.was_found():
+            print("Locating the position of the International Space Station...")
+            time.sleep(3)
+            print(f"The position of the International Space Station is: {self.position})
+            print(f"It is moving at a speed of about {self.speed} km per second")
+        else:
+            print("Unable to locate the ISS")
+    
+            
+    def distance_between(self, second_position):
+        if self.was_found():
+            print("Calculating distance...")
+            time.sleep(3)
+            distance_km = distance.haversine(self.position, second_position)
+            print(f"The International Space Station is about {distance_km} km from you")
+        else:
+            print("Unable to locate the ISS)
+
+
+    
 """
