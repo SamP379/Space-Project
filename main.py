85ea559055124992a942b24a4b3598c221954c47
Sam Patel <sampatel379@gmail.com>
2025-04-04 14:13:56 +0100

    Cleaned up code

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
+"""
+
 
 from iss import ISS
 from user import User
 
 
 
+
 def iss_routine():
-    
     iss = ISS()
-    print("")
     user_address = input("Enter your address: ")
     user = User(user_address)
-
     user.display_position()
     iss.display_status()
-     
-    # Calculating distance...
-    # The International Space Station is about 4320 KM from you
-
-
-
-def main():
-    iss_routine()
-
-    
-   
-main()
+    iss.display_distance(user.get_position())
+ 
+"""   
