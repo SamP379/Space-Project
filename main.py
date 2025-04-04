0d11707b679d104a906886b3450453ba44e56bd3
Sam Patel <sampatel379@gmail.com>
2025-04-04 12:27:01 +0100

    Cleaned up main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 
-"""
+from iss import ISS
+from user import User
+
+
+
+def iss_routine():
+    
+    iss = ISS()
+    print("")
+    user_address = input("Enter your address: ")
+    user = User(user_address)
+
+    user.display_position()
+    iss.display_status()
+     
+    # Calculating distance...
+    # The International Space Station is about 4320 KM from you
+
 
 
 def main():
+    iss_routine()
 
-    user_address = input("Enter your address: ")
-    user = User(user_address)
-    user.display_position()
     
-    iss = ISS()
-    iss.display_status()
    
-
-"""
-
-
-# Enter your address: 47 Chipping Vale, Emerson Valley, Milton Keynes
-# Locating your position...
-# Your position is: (53.234, -723434)
-     
-# Locating the position of the International Space Station...
-# The position of the International Space Station is: (3.1224, -1.1324)
-# The International Space Station is somewhere over Ohio, USA
-     
-# Calculating distance...
-# The International Space Station is about 4320 KM from you
+main()
